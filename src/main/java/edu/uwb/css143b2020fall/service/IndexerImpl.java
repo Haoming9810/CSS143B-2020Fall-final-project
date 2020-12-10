1a6cd56569923debe3807cb361e7f1cb08f51a68
cici1 <1249819124@qq.com>
2020-12-10 01:32:28 -0800

    part

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -2,16 +2,35 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        // add your code
+        LinkedList words = new LinkedList();
+        for (int i = 0; i < docs.size(); i++) {
+            String[] word = docs.get(i).split("\\s+");
+            for (int j = 0; j < docs.size(); j++) {
+                if (!words.contains(word[j])) {
+                    words.add(word[j]);
+                }
+            }
+
+            for (int k = 0; k < words.size(); k++) {
+
+                if (!words.contains(word[k])) {
+                    List<List<Integer>> FirstList = new ArrayList<>();
+                    indexes.put(word[k], FirstList);
+                    for(int p = 0; p< words.size(); p++){
+                        List<Integer> SecondList = new ArrayList<>();
+                        FirstList.add(SecondList);
+                    }
+                }
+                indexes.get(word[k]).get(i).add(k);
+            }
+
+        }
         return indexes;
     }
 }
