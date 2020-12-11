55c80f1b2971c4c833d1d6aa2ed0e9098bc62c32
cici1 <1249819124@qq.com>
2020-12-10 16:50:32 -0800

    part

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -2,15 +2,15 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
-        // add your code
+        Map<String, List<List<Integer>>> WordIdx = new HashMap<>();
+
+
         return result;
     }
 }
